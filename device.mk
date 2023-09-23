3f2e19fd61b14c927aeed169e38a6dd41e644010
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-09-23 08:34:17 +0700

    RMX1801: Initial Adaptation For Android 12.1
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+# Copyright (C) 2018 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,5 +14,7 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_RMX1801.mk
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    charger
--- twrp_RMX1801.mk
+++ twrp_RMX1801.mk
@@ -14,22 +14,24 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := RMX1801
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, build/target/product/embedded.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from Realme RMX1801
+$(call inherit-product, device/realme/RMX1801/device.mk)
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    charger
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX1801
-PRODUCT_NAME := omni_RMX1801
+PRODUCT_NAME := twrp_RMX1801
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := RMX1801
 PRODUCT_MANUFACTURER := OPPO
